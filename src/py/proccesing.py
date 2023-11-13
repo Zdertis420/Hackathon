795809fa76afd427519bd066962636e6a1556bc5
licomnaklavy <129503682+licomnaklavy@users.noreply.github.com>
2023-11-13 10:30:19 +0300

    Update proccesing.py
    Красота

--- src/py/proccesing.py
+++ src/py/proccesing.py
@@ -1,70 +1,85 @@
 from os import listdir
-
-# Все файлы из папки
-files = [f for f in listdir("C:\\Users\\andre\Documents\Хакатон\docs\docs\\utf8")]
-
-# Преобразование документов в списки
-lists = []
-
-for file in files:
-    with open(f"C:\\Users\\andre\Documents\Хакатон\docs\docs\\utf8\\{file}", mode = 'r', encoding = 'utf-8') as f:
-        f = [value for value in f.readlines() if value != "\n"]
-        lists.append(f)
-
-# Срезание у строк списков \n в конце и перевод в верхний регистр
-for list in lists:
-    for i in range(len(list)):
-        list[i] = list[i][:-1]
-        list[i] = list[i].upper()
-
-# Удаление мусора из всех строк
-for list in lists:
-    for string in range(len(list)):
-        for symb in list[string]:
-            n = ord(symb)
-            if not (n >= 1040 and n <= 1071) or n == 1025 or n == 32:
-                list[string] = list[string].replace(symb, " ", 1)
-
-# Цифры сверху в цикле это Юникод крайних русских букв и пару исключений
-# print(ord("А"), ord("Я"), ord("Ё"), ord(" "))
-
-
-# Создание новых списков со всеми словами из документа
-lists_words = []
-
-for list in lists:
-    words = []
-    for string in list:
-        if string:
-            words.extend(string.split())
-    if words:
-        lists_words.append(words)
-
-# Перевод всех слов в начальную форму и удаление служебных частей речи.
-final_lists = []
-s = []
-
-with open("C:\\Users\\andre\Documents\Хакатон\\stopwords-ru.txt", mode = 'r', encoding = 'utf-8') as f:
-    stopwords = [i.strip() for i in f.readlines()]
-
 import pymorphy2
-morph = pymorphy2.MorphAnalyzer()
 
-for list in lists_words:
+#############################################################################################################################################################################
+
+def process_func(path):
+
+    # Все файлы из папки ####################################################################################################################################################
+    files = [f for f in listdir(f"{path}/docs/utf8")]
+
+    # Преобразование документов в списки #####################################################################################################################################
+    lists = []
+
+    for file in files:
+        with open(f"{path}//docs/utf8/{file}", mode='r', encoding='utf-8') as f:
+            f = [value for value in f.readlines() if value != "\n"]
+            lists.append(f)
+
+    # Срезание у строк списков \n в конце и перевод в верхний регистр ########################################################################################################
+    for list in lists:
+        for i in range(len(list)):
+            list[i] = list[i][:-1]
+            list[i] = list[i].upper()
+
+    # Удаление мусора из всех строк ##########################################################################################################################################
+    for list in lists:
+        for string in range(len(list)):
+            for symb in list[string]:
+                n = ord(symb)
+                if not (n >= 1040 and n <= 1071) or n == 1025 or n == 32:
+                    list[string] = list[string].replace(symb, " ", 1)
+
+    # Цифры сверху в цикле это Юникод крайних русских букв и пару исключений
+    # print(ord("А"), ord("Я"), ord("Ё"), ord(" "))
+
+    # Создание новых списков со всеми словами из документа ##################################################################################################################
+    lists_words = []
+
+    for list in lists:
+        words = []
+        for string in list:
+            if string:
+                words.extend(string.split())
+        if words:
+            lists_words.append(words)
+
+    # Перевод всех слов в начальную форму. #################################################################################################################################
+    final_lists = []
     s = []
-    for word in range(len(list)):
-        if list[word].lower() not in stopwords:
-            s.append(morph.parse(list[word])[0].normal_form.upper())
-    final_lists.append(s)
 
-# Создание словаря со всеми ответами
-words_dict = {}
-answer_list = []
+    with open(f"{path}/stopwords-ru.txt", mode='r', encoding='utf-8') as f:
+        stopwords = [i.strip() for i in f.readlines()]
 
-for list in final_lists:
-    for word in list:
-        words_dict[word] = list.count(word)
-    answer_list.append(words_dict)
-    words_dict = {}
+    morph = pymorphy2.MorphAnalyzer()
 
-print(answer_list)
+    for list in lists_words:
+        s = []
+        for word in range(len(list)):
+            if list[word].lower() not in stopwords:
+                s.append(morph.parse(list[word])[0].normal_form.upper())
+        final_lists.append(s)
+
+
+    return final_lists
+
+
+############################################################################################################################################################################
+
+path = "/home/user/Загрузки/"
+# path = input()
+
+print(process_func(path))
+
+
+# # Создание словаря со всеми ответами
+# words_dict = {}
+# answer_list = []
+#
+# for list in final_lists:
+#     for word in list:
+#         words_dict[word] = list.count(word)
+#     answer_list.append(words_dict)
+#     words_dict = {}
+#
+# print(answer_list)
