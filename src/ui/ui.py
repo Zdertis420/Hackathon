ae64a575e2c87eeab63e8cd9991e1bb0d4eea376
Zdertis420 <AnRobo07@yandex.ru>
2023-11-13 21:02:01 +0300

    Made base for GUI

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,1 +1,18 @@
-import tkinter
+import sys
+
+from PyQt6.QtWidgets import QMainWindow, QApplication
+
+
+class MainWin(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Hackathon")
+        self.setGeometry(710, 300, 500, 500)
+
+
+app = QApplication(sys.argv)
+mainWindow = MainWin()
+
+mainWindow.show()
+sys.exit(app.exec())
