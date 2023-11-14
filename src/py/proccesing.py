c916b94c391266a3243af8ab836bb6b3c470447f
12nikita05 <125774727+12nikita05@users.noreply.github.com>
2023-11-14 12:23:45 +0300

    Update proccesing.py

--- src/py/proccesing.py
+++ src/py/proccesing.py
@@ -1,47 +1,35 @@
 from os import listdir
 import pymorphy3
-
-from tkinter import *
+import numpy as np
+from string import punctuation
 from tkinter import filedialog
-
+import time
+start_time = time.time()
 #############################################################################################################################################################################
 
 def process_func(path):
 
     # Все файлы из папки ####################################################################################################################################################
-    files = [f for f in listdir(f"{path}")]
-
+    files = np.array([f for f in listdir(f"{path}")])
     # Преобразование документов в списки #####################################################################################################################################
-    lists = []
-
+    lists = np.array([])
     for file in files:
         with open(f"{path}/{file}", mode='r', encoding='utf-8') as f:
-            f = [value for value in f.readlines() if value != "\n"]
-            lists.append(f)
-
-    # Срезание у строк списков \n в конце и перевод в верхний регистр ########################################################################################################
-    for list in lists:
-        for i in range(len(list)):
-            list[i] = list[i][:-1]
-            list[i] = list[i].upper()
-
+            lists = np.append(lists, [[value.rstrip().upper() for value in f.readlines() if value != "\n"]])
     # Удаление мусора из всех строк ##########################################################################################################################################
-    for list in lists:
-        for string in range(len(list)):
-            for symb in list[string]:
-                n = ord(symb)
-                if not (n >= 1040 and n <= 1071) or n == 1025 or n == 32:
-                    list[string] = list[string].replace(symb, " ", 1)
+    # translator = str.maketrans("", "", punctuation)
+    # remove_punctuation = np.vectorize(lambda x: x.translate(translator)) # Если потребуется объяснить
+    # lists = remove_punctuation(lists)
 
-    # Цифры сверху в цикле это Юникод крайних русских букв и пару исключений
-    # print(ord("А"), ord("Я"), ord("Ё"), ord(" "))
-
+    lists = (np.vectorize(lambda x: x.translate(str.maketrans("", "", punctuation)))(lists))
+    print(lists)
+    print("--- %s seconds ---" % (time.time() - start_time))
     # Создание новых списков со всеми словами из документа ##################################################################################################################
     lists_words = []
 
-    for list in lists:
+    for doc in lists:
         words = []
-        for string in list:
+        for string in doc:
             if string:
                 words.extend(string.split())
         if words:
@@ -91,4 +79,5 @@ for list in final_lists:
     answer_list.append(sorted(words_dict.items(), key = lambda items: items[1], reverse = True))
     words_dict = {}
 
-print(answer_list)
+print(len(answer_list))
+print("--- %s seconds ---" % (time.time() - start_time))
